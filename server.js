2b6bf4e87fc6698d12176b4d1d49591a6abd9683
Gabriel Farrell <g@gsf.io>
2015-12-08 22:07:39 -0500

    Mail admins after upload

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ var fs = require('fs');
 var http = require('http');
 var moment = require('moment');
 var mustache = require('mustache');
+var nodemailer = require('nodemailer');
+var url = require('url');
 
-var port = process.env.PORT || 1042
+var port = process.env.PORT || 1042;
 var template = fs.readFileSync('index.html', 'utf8');
+var transporter = nodemailer.createTransport({
+  service: process.env.SMTP_SERVICE,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+});
 
 http.createServer(function (req, res) {
   var acl = 'public-read';
@@ -26,6 +35,22 @@ http.createServer(function (req, res) {
   var hmac = crypto.createHmac('sha1', process.env.SECRET_KEY);
   hmac.update(policy);
 
+  // Mail admins on get with key param
+  var query = url.parse(req.url, true).query;
+  var fileUrl = query && query.key && 'https://data.wavefarm.org/' + query.key;
+
+  var opt = {
+    from: 'Wave Farm <info@wavefarm.org>',
+    subject: 'Wave Farm upload',
+    to: 'info@wgxc.org,archive@wgxc.org,info@wavefarm.org',
+    text: 'A file has been uploaded to data.wavefarm.org.\n\nURL: ' + fileUrl + '\n'
+  };
+
+  transporter.sendMail(opt, function (err, info) {
+    if (err) return console.error(err)
+    console.log('Email sent: ' + info.response)
+  });
+
   var view = {
     path: moment().format('YYYYMMDDHHmmss'),
     accessKey: process.env.ACCESS_KEY,
@@ -33,8 +58,10 @@ http.createServer(function (req, res) {
     acl: acl,
     successActionRedirect: successActionRedirect,
     policy: policy,
-    signature: hmac.digest('base64')
+    signature: hmac.digest('base64'),
+    url: fileUrl
   };
+
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end(mustache.render(template, view));
 }).listen(port, function () {
